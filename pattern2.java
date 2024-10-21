91910b5ce0fcdcca9439c19f74e9ed80ffcc8b20
Mantharapu Joshitha Maha Lakshmi <22A91A05J0@aec.edu.in>
2024-10-21 09:30:45 +0530

    patterns

--- pattern2.java
+++ pattern2.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class pattern1 {
+public class pattern2 {
     public static void main (String args[])
     {
         Scanner sc=new Scanner (System.in);
@@ -9,7 +9,14 @@ public class pattern1 {
         {
             for(int j=1;j<=m;j++)
             {
-                System.out.print("*");
+                if(i==1 || i==n || j==1 || j==m)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
