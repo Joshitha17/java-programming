4af713f8129353099f5f72665fb3632de5881d8d
Mantharapu Joshitha Maha Lakshmi <22A91A05J0@aec.edu.in>
2024-10-23 22:09:58 +0530

    more patterns

--- pattern10.java
+++ pattern10.java
@@ -1,5 +1,39 @@
 public class pattern10 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int n=4;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            int patterns=2*(n-i);
+            for(int j=1;j<=patterns;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            int patterns=2*(n-i);
+            for(int j=1;j<=patterns;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
