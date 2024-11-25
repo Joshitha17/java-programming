c2599271ef124d72a604b7b014b223a75d00b687
Mantharapu Joshitha Maha Lakshmi <22A91A05J0@aec.edu.in>
2024-11-25 16:49:45 +0530

    .

--- practice.java
+++ practice.java
@@ -12,7 +12,7 @@ public class practice {
             arrays[i]=sc.nextInt();
 
         }
-        for(int i=0;i<n;i++)
+        for(int i=0;i<arrays.length;i++)
         {
             if(arrays[i]==element)
             {
