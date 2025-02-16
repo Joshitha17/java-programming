c1fb5472e41e564bd40b74eab7047e80b0e7d2e1
Mantharapu Joshitha Maha Lakshmi <22A91A05J0@aec.edu.in>
2025-02-16 11:14:22 +0530

    2d arrays

---
+++ array2d_problem.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class array2d_problem {
+    public static void main (String args [])
+    {
+        Scanner sc = new Scanner(System.in);
+        int row=sc.nextInt();
+        int col=sc.nextInt();
+        int search=4;
+        int [][] arr=new int[row][col];
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(arr[i][j]==search)
+                {
+                    System.out.println("element found at position "+i+" "+j);
+                }
+            }
+            System.out.println();   
+        }
+    }
+}
